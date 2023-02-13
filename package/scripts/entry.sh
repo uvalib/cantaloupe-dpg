64f82e3122218812a25cebb39c556de3dbc92103
Dave Goldstein <dpg3k@virginia.edu>
2023-02-13 07:39:09 -0500

    Use path for the java binary

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -2,7 +2,7 @@
 # startup script
 #
 
-cd bin; /usr/local/openjdk-11/bin/java -Dcantaloupe.config=cantaloupe.properties -Xmx4g -jar cantaloupe.jar
+cd bin; java -Dcantaloupe.config=cantaloupe.properties -Xmx4g -jar cantaloupe.jar
 
 #
 # end of file
